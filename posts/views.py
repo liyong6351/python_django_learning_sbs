8b16db9235aba7147e699157971c59ba35a2a811
liyong <liyong@163.com>
2020-03-20 17:02:02 +0800

    优化404问题

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 # -*-coding:utf-8 -*-
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from posts.models import Article
 
@@ -8,9 +8,12 @@ from posts.models import Article
 # Create your views here.
 # 创建一个获取文章详情的方法
 def article_detail(request, article_id):
-    try:
-        article = Article.objects.get(id=article_id)
-    except Article.DoesNotExist:
-        raise Http404("文章不存在")
-    else:
-        return HttpResponse("文章标题:%s <br> 文章内容:%s" % (article.title, article.content))
+    #    try:
+    #        article = Article.objects.get(id=article_id)
+    #        context = {"article": article}
+    #        return render(request, "article_detail.html", context)
+    #    except Article.DoesNotExist:
+    #        raise Http404("文章不存在")
+    article = get_object_or_404(Article, id=article_id)
+    context = {"article": article}
+    return render(request, "article_detail.html", context)
